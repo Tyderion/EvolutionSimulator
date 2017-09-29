1e829b2a78f38a17c2675a51e60bcea4b2fe60d3
Gabriel Nadler <nadlergabriel@gmail.com>
2017-09-29 19:49:13 +0200

    Add Enum for "menu"

---
+++ src/evolution/Menu.java
@@ -0,0 +1,24 @@
+package evolution;
+
+public enum Menu {
+    START_SCREEN(0),
+    MAIN_SCREEN(1),
+    CREATE_CREATURES(2),
+    VIEW_GENERATED(3),
+    START_SIMULATION(4),
+    SIMULATION_RUNNING(5),
+    CLEANUP_SIMULATION(6),
+    SIMULATION_RESULT(7),
+    SIMULATION_SORT_VIS(8),
+    SIMULATION_RESULT_SORTED(9),
+    KILL_500(10),
+    KILLS_VISUALIZE(11),
+    REPRODUCE(12),
+    SHOW_NEW_CREATURES(13);
+
+    private final int menu;
+
+    Menu(int menu) {
+        this.menu = menu;
+    }
+}
